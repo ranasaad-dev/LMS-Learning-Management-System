03f68c67b68fe1089234a99004da9417c4fb411d
ranasaad-dev <ranasaad.dev@gmail.com>
2026-04-06 01:56:49 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+ server: {
+    allowedHosts: [
+      'lms-production-62a6.up.railway.app', // add your host here
+      // you can also add more hosts if needed
+    ],
+  },
 })
