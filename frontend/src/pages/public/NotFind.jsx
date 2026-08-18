b85e8cd5d4d535df456a75b2c25913c106986961
Rana Saad <ranasaad.dev@gmail.com>
2026-08-19 04:03:33 +0500

    uploaded notfound.jsx

---
+++ frontend/src/pages/public/NotFind.jsx
@@ -0,0 +1,11 @@
+import './NotFound.css';
+function NotFound (){
+return(
+    <div className='not-found'>
+        <h2 className='not-found-heading'>
+            404 Not Found
+            </h2>
+    </div>
+)
+}
+export default NotFound;
